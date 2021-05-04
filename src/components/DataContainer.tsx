46397803ebee1c368badba88219daaab345ffb70
Zack <TheZackMatthews>
2021-05-04 14:18:38 -0700

    Fix: Increased stability of DataContainer and Swiper
    Occasionally, education chart will still be empty, but this is usually when we try to break it

--- src/components/DataContainer.tsx
+++ src/components/DataContainer.tsx
@@ -7,125 +7,136 @@ import { createChartRequest, fetchCensusData } from '../helpers/Helpers';
 import ChartSwiper from './Swiper';
 import { Feature, Polygon, Properties } from '@turf/turf';
 
-type AnyObject = { [key: string]: any };
-
-const createSummaryData = (
-  data: {[key: string]:EducationCategories},
-  varMap: AnyObject,
-  sumVars: {[key: string]: string[]},
-  reCalc?: boolean
-) => {
-  const summary = new CensusSummary(data, varMap);
-  if (!reCalc) summary.mapDataToDescriptor();
-  summary.getTotals();
-  Object.entries(sumVars).forEach(([key, valArr]) => {
-    summary.sumShares(valArr, key);
-  });
-  return summary;
+interface Props {
+  onScreen: Feature<Polygon, Properties>[] | undefined;
+	setShowDataContainer: React.Dispatch<React.SetStateAction<Boolean>>;
+	showDataContainer: Boolean;
 };
 
-const DataContainer = ({ onScreen, setShowDataContainer, showDataContainer }: { onScreen: Feature<Polygon, Properties>[] | undefined, setShowDataContainer:React.Dispatch<React.SetStateAction<Boolean>>, showDataContainer:Boolean}) => {
-  const [summary, setSummary] = useState({ race: {}, education: {} });
-  const [data, setData] = useState<AnyObject>({});
-  const [finishedLoad, setFinishedLoad] = useState<Boolean>(false);
+type AnyObject = { [key: string]: any };
 
+// Function that makes calls to /helpers. Kind of like a Summary class
+const createSummaryData = (
+	data: { [key: string]: EducationCategories },
+	varMap: AnyObject,
+	sumVars: { [key: string]: string[] },
+	reCalc?: boolean
+) => {
+	const summary = new CensusSummary(data, varMap);
+	if (!reCalc) summary.mapDataToDescriptor();
+	summary.getTotals();
+	Object.entries(sumVars).forEach(([key, valArr]) => {
+		summary.sumShares(valArr, key);
+	});
+	return summary;
+};
 
-  useEffect(() => {
-    const raceTables = Object.keys(raceVars).map((item) => item.split('_')[1]);
-    const raceRequest = createChartRequest('B03002', raceTables);
-    const edTables = Object.keys(edVars).map((item) => item.split('_')[1]);
-    const edRequest = createChartRequest('C15003', edTables);
+const DataContainer = ({ onScreen, setShowDataContainer, showDataContainer }: Props) => {
+	const [summary, setSummary] = useState({ race: {}, education: {} });
+	const [data, setData] = useState<AnyObject>({});
+	const [ethnicDataLoad, setEthnicDataLoad] = useState<Boolean>(false);
+	const [educationDataLoad, setEducationDataLoad] = useState<Boolean>(false);
 
-    fetchCensusData(raceRequest).then((result) => {
-      const raceSummary = createSummaryData(result.geoIdValue, raceVars, {
-        Other: ['Native', 'Pacific', 'Multi', 'Other'],
-      });
-      delete raceSummary.shares['Total'];
-      setData((prevData) => ({ ...prevData, race: raceSummary.data }));
-      setSummary(() => ({
-        ...summary,
-        race: raceSummary.shares }));
-      setFinishedLoad(true);
-    });
+	useEffect(() => {
+		const raceTables = Object.keys(raceVars).map((item) => item.split('_')[1]);
+		const raceRequest = createChartRequest('B03002', raceTables);
+		const edTables = Object.keys(edVars).map((item) => item.split('_')[1]);
+		const edRequest = createChartRequest('C15003', edTables);
 
-    fetchCensusData(edRequest).then((result) => {
-      const edSummary = createSummaryData(result.geoIdValue, edVars, {
-        'High School': ['High School', 'GED'],
-        'Some College': ['1 Y College', '1+ Y College', 'Associates'],
-        Graduate: ['Master\'s', 'Professional', 'Doctorate'],
-      });
-      edSummary.shares['No Degree'] =
-      edSummary.shares['Total'] -
-      edSummary.shares['High School'] -
-      edSummary.shares['Some College'] -
-      edSummary.shares['Graduate'] -
-      edSummary.shares['Bachelor\'s'];
-      delete edSummary.shares['Total'];
-      setData((prevData) => ({ ...prevData, education: edSummary.data }));
-      setSummary((prevData) => ({
-        ...prevData,
-        education: edSummary.shares,
-      }));
-    });
-  }, []);
+		fetchCensusData(raceRequest).then((result) => {
+			const raceSummary = createSummaryData(result.geoIdValue, raceVars, {
+				Other: ['Native', 'Pacific', 'Multi', 'Other'],
+			});
+			delete raceSummary.shares['Total'];
+			setData((prevData) => ({ ...prevData, race: raceSummary.data }));
+			setSummary(() => ({
+				...summary,
+				race: raceSummary.shares,
+			}));
+			setEthnicDataLoad(true);
+		});
 
-  useEffect(() => {
-    if (onScreen && Object.keys(data).length) {
-      const onScreenGeoIDs = [];
-      for (const item of onScreen) {
-        const geoId = item.properties?.GEO_ID.split('US')[1];
-        onScreenGeoIDs.push(geoId);
-      }
-      const onScreenRace = Object.assign({},
-        ...onScreenGeoIDs.map((key) => ({
-          [key]: (data.race) ? data.race[key] : {},
-        }))
-      );
+		fetchCensusData(edRequest).then((result) => {
+			const edSummary = createSummaryData(result.geoIdValue, edVars, {
+				'High School': ['High School', 'GED'],
+				'Some College': ['1 Y College', '1+ Y College', 'Associates'],
+				Graduate: ["Master's", 'Professional', 'Doctorate'],
+			});
+			edSummary.shares['No Degree'] =
+				edSummary.shares['Total'] -
+				edSummary.shares['High School'] -
+				edSummary.shares['Some College'] -
+				edSummary.shares['Graduate'] -
+				edSummary.shares["Bachelor's"];
+			delete edSummary.shares['Total'];
+			setData((prevData) => ({ ...prevData, education: edSummary.data }));
+			setSummary((prevData) => ({
+				...prevData,
+				education: edSummary.shares,
+			}));
+			setEducationDataLoad(true);
+		});
+	}, []);
 
-      const onScreenEd = Object.assign({},
-        ...onScreenGeoIDs.map((key) => ({
-          [key]: (data.education) ? data.education[key] : {},
-        }))
-      );
-      const raceSummary = createSummaryData(
-        onScreenRace,
-        raceVars,
-        {
-          Other: ['Native', 'Pacific', 'Multi', 'Other'],
-        },
-        true
-      );
-      delete raceSummary.shares['Total'];
-      setSummary((prevData) => ({ ...prevData, race: raceSummary.shares }));
+	useEffect(() => {
+		if (onScreen && Object.keys(data).length) {
+			const onScreenGeoIDs = [];
+			for (const item of onScreen) {
+				const geoId = item.properties?.GEO_ID.split('US')[1];
+				onScreenGeoIDs.push(geoId);
+			}
+			const onScreenRace = Object.assign(
+				{},
+				...onScreenGeoIDs.map((key) => ({
+					[key]: data.race ? data.race[key] : {},
+				}))
+			);
 
-      const edSummary = createSummaryData(
-        onScreenEd,
-        edVars,
-        {
-          'High School': ['High School', 'GED'],
-          'Some College': ['1 Y College', '1+ Y College', 'Associates'],
-          Graduate: ['Master\'s', 'Professional', 'Doctorate'],
-        },
-        true
-      );
-      edSummary.shares['No Degree'] =
-edSummary.shares['Total'] -
-edSummary.shares['High School'] -
-edSummary.shares['Some College'] -
-edSummary.shares['Graduate'] -
-edSummary.shares['Bachelor\'s'];
-      delete edSummary.shares['Total'];
-      setSummary((prevData) => ({ ...prevData, education: edSummary.shares }));
-    }
-  }, [onScreen]);
+			const onScreenEd = Object.assign(
+				{},
+				...onScreenGeoIDs.map((key) => ({
+					[key]: data.education ? data.education[key] : {},
+				}))
+			);
+			const raceSummary = createSummaryData(
+				onScreenRace,
+				raceVars,
+				{
+					Other: ['Native', 'Pacific', 'Multi', 'Other'],
+				},
+				true
+			);
+			delete raceSummary.shares['Total'];
+			setSummary((prevData) => ({ ...prevData, race: raceSummary.shares }));
 
-  return summary.race && summary.education && onScreen && finishedLoad ? (
-    <div id="dataContainer" className={showDataContainer ? "dataContainer" : "hidden"}>
-      <ChartSwiper data = {summary} setShowDataContainer={setShowDataContainer}/>
-    </div>
-  ) : (
-    <h3>Data is loading</h3>
-  );
+			const edSummary = createSummaryData(
+				onScreenEd,
+				edVars,
+				{
+					'High School': ['High School', 'GED'],
+					'Some College': ['1 Y College', '1+ Y College', 'Associates'],
+					Graduate: ["Master's", 'Professional', 'Doctorate'],
+				},
+				true
+			);
+			edSummary.shares['No Degree'] =
+				edSummary.shares['Total'] -
+				edSummary.shares['High School'] -
+				edSummary.shares['Some College'] -
+				edSummary.shares['Graduate'] -
+				edSummary.shares["Bachelor's"];
+			delete edSummary.shares['Total'];
+			setSummary((prevData) => ({ ...prevData, education: edSummary.shares }));
+		}
+	}, [onScreen]);
+
+	return onScreen && ethnicDataLoad && educationDataLoad ? (
+		<div id="dataContainer" className={showDataContainer ? 'dataContainer' : 'hidden'}>
+			<ChartSwiper data={summary} setShowDataContainer={setShowDataContainer} />
+		</div>
+	) : (
+		<h3>Data is loading</h3>
+	);
 };
 
 export default DataContainer;
--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -19,63 +19,64 @@ interface SummeryData {
 		race: AnyObject;
 		education: AnyObject;
 	};
-  setShowDataContainer: React.Dispatch<React.SetStateAction<Boolean>>
+	setShowDataContainer: React.Dispatch<React.SetStateAction<Boolean>>;
 }
 
 SwiperCore.use([Navigation]);
-const ChartSwiper = (props: SummeryData) => {
-  const [raceChart, setRaceChart] = useState<AnyObject>();
-  const [edChart, setEdChart] = useState<AnyObject>();
+const ChartSwiper = ({ data, setShowDataContainer }: SummeryData) => {
+	const [raceChart, setRaceChart] = useState<AnyObject>();
+	const [edChart, setEdChart] = useState<AnyObject>();
 
-  useEffect(() => {
-    if (props.data) {
-      setRaceChart(drawChart(props.data.race, '#race-chart'));
-      setEdChart(drawChart(props.data.education, '#education-chart'));
-    }
-  }, []);
+	useEffect(() => {
+		if (data) {
+			setRaceChart(drawChart(data.race, '#race-chart'));
+			setEdChart(drawChart(data.education, '#education-chart'));
+		}
+	}, []);
 
-  useEffect(() => {
-    if (raceChart) {
-      // Here we take steps to correct any errors in the data before we pass it to the chart
-      const currentValue = Object.assign(
-        {
-          Asian: 0,
-          Black: 0,
-          Hispanic: 0,
-          Other: 0,
-          White: 0,
-        },
-        props.data.race
-      );
-      let totalCount = 0;
-      for (const key in currentValue) {
-        if (!currentValue[key]) currentValue[key] = 0; // Correct any null or undefined values
-        totalCount += currentValue[key];
-      }
-      raceChart.update(currentValue);
-      // If total count is zero, we're not graphing anything, so unmount the DataContainer
-      if (totalCount === 0) props.setShowDataContainer(false);
-    }
+	useEffect(() => {
+		if (raceChart) {
+			// Here we take steps to correct any errors in the data before we pass it to the chart
+			const currentValue = Object.assign(
+				{
+					Asian: 0,
+					Black: 0,
+					Hispanic: 0,
+					Other: 0,
+					White: 0,
+				},
+				data.race
+			);
+			let totalCount = 0;
+			for (const key in currentValue) {
+				if (!currentValue[key]) currentValue[key] = 0; // Correct any null or undefined values
+				totalCount += currentValue[key];
+			}
+			raceChart.update(currentValue);
+			// If total count is zero, we're not graphing anything, so unmount the DataContainer
+			if (totalCount === 0) setShowDataContainer(false);
+		}
 
-    if (edChart) {
-      // Here we take steps to correct any errors in the data before we pass it to the chart
-      const currentValue = Object.assign(
-        {
-          'Bachelor\'s': 0,
-          'Graduate': 0,
-          'High School': 0,
-          'No Degree': 0,
-          'Some College': 0,
-        },
-        props.data.education
-      );
-      for (const key in currentValue) {
-        if (!currentValue[key]) currentValue[key] = 0;
-      }
-      edChart.update(currentValue);
-    }
-  }, [props.data]);
+		if (edChart) {
+			// Here we take steps to correct any errors in the data before we pass it to the chart
+			const currentValue = Object.assign(
+				{
+					"Bachelor's": 0,
+					Graduate: 0,
+					'High School': 0,
+					'No Degree': 0,
+					'Some College': 0,
+				},
+				data.education
+			);
+			for (const key in currentValue) {
+				if (!currentValue[key]) currentValue[key] = 0;
+			}
+			edChart.update(currentValue);
+		}
+	}, [data]);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   //  useEffect(() => {
   //console.log(props.closeChart);
@@ -213,6 +214,23 @@ const ChartSwiper = (props: SummeryData) => {
 =======
   } else return null;
 >>>>>>> 5011befb... Added Eslint, auto fixed errors
+=======
+	return (
+		<React.Fragment>
+			<Swiper id="main" tag="section" wrapperTag="ul" navigation pagination spaceBetween={0} slidesPerView={1}>
+				<div className="swiper-wrapper">
+					<div className="swiper-slide" id="race-chart" key={1} data-tag="li">
+						<div className="swiper-title">Ethnicity</div>
+					</div>
+					<div className="swiper-slide" id="education-chart" key={2} data-tag="li">
+						{' '}
+						<div className="swiper-title">Education</div>
+					</div>
+				</div>
+			</Swiper>
+		</React.Fragment>
+	);
+>>>>>>> 73eba49d... Fix: Increased stability of DataContainer and Swiper
 };
 
 export default ChartSwiper;
